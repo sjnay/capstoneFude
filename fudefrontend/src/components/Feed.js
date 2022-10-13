a1b3adbf8f94b3f5cdb6a6b1674151a5bfdc0f2d
sjnay <samanthajnay@gmail.com>
2022-10-13 12:15:04 -0500

    starting user auth

--- fudefrontend/src/components/Feed.js
+++ fudefrontend/src/components/Feed.js
@@ -3,14 +3,13 @@ function Feed(props){
         props.deletion(props.id)
     }
     return(
-
-        <div className='post-container'>
+<div>
+<div className='post-container'>
 <h1>Place: {props.place_name}</h1>
 <p>Food: {props.food_name}</p>
-<span className="material-symbols-outlined"
-
-onClick={handleClick}>delete</span>
-        </div>
+</div>
+<span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+</div>     
     )
 }
 
